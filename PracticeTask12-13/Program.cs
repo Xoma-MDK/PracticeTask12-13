8915dfd7decd7be7acae74f9c5b41e4b2e173369
Daniil Molotkov <molotkov.58@ya.ru>
2022-01-14 11:17:57 +0700

    Обработка массивов

--- PracticeTask12-13/Program.cs
+++ PracticeTask12-13/Program.cs
@@ -13,8 +13,35 @@ namespace PracticeTask12_13
         {
             return Fact(n) / (Fact(m)*Fact(n-m));
         }
+        static void printArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]}");
+            }
+            Console.WriteLine();
+        }
+        static int[] Max_array(int[] array)
+        {
+            int max = 0;
+            int index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    index = i;
+                }
+            }
+            for (int i = index + 1; i < array.Length;i++)
+            {
+                array[i] = 1;
+            }
+            return array;
+        }
         static void Main(string[] args)
         {
+            Random random = new Random();
             Console.Write("Введите n: ");
             int n = int.Parse(Console.ReadLine());
             Console.Write("Введите m: ");
@@ -27,6 +54,23 @@ namespace PracticeTask12_13
             {
                 Console.WriteLine("ошибка: n должно быть больше m");
             }
+            Console.Write("Введите количество элементов в массиве: ");
+            int N = int.Parse(Console.ReadLine());
+            int[] A = new int[N];
+            int[] B = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = random.Next(1, 100);
+                B[i] = random.Next(1, 100);
+            }
+            Console.Write("Массив A до обработки:");
+            printArray(A);
+            Console.Write("Массив B до обработки:");
+            printArray(B);
+            Console.Write("Массив A после обработки:");
+            printArray(Max_array(A));
+            Console.Write("Массив B после обработки:");
+            printArray(Max_array(B));
         }
     }
 }
