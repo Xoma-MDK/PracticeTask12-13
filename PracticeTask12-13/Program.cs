9d0e9b8137d82217e7461aef736c6124620040d6
Daniil Molotkov <molotkov.58@ya.ru>
2022-01-14 10:33:52 +0700

    Добавьте файлы проекта.

---
+++ PracticeTask12-13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeTask12_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
