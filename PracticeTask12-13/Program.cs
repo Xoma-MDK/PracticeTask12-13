3a0bd9f8051b277094ea8be892e1238dc3ca72a9
Daniil Molotkov <molotkov.58@ya.ru>
2022-01-14 10:50:08 +0700

    Написано решение

--- PracticeTask12-13/Program.cs
+++ PracticeTask12-13/Program.cs
@@ -4,9 +4,29 @@ namespace PracticeTask12_13
 {
     internal class Program
     {
+        static int Fact(int a)
+        {
+            if (a == 0) return 1;
+            return a * Fact(a - 1);
+        }
+        static int Combinations(int n, int m)
+        {
+            return Fact(n) / (Fact(m)*Fact(n-m));
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Введите m: ");
+            int m = int.Parse(Console.ReadLine());
+            if (n > m)
+            {
+                Console.WriteLine($"Число сочетаний из n по m: {Combinations(n, m)}");
+            }
+            else
+            {
+                Console.WriteLine("ошибка: n должно быть больше m");
+            }
         }
     }
 }
